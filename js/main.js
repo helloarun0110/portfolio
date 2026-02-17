3046af70b5f41100baf69471d49ff85a328e33f4
Arun <helloarun0110@gmail.com>
2026-02-18 04:39:28 +0600

    complete --- and also add the first the resume build task here in the view resume option(code are in the resume dir section )

--- js/main.js
+++ js/main.js
@@ -1,37 +1,15 @@
-// const input = document.getElementById("mediaInput");
-// const preview = document.getElementById("preview");
 
-// if (input) {
-//     input.addEventListener("change", () => {
-//         preview.innerHTML = "";
+const readMoreBtn = document.querySelector(".read-more-btn");
+const moreText = document.querySelector(".more-text");
 
-//         const file = input.files[0];
-//         if (!file) return;
+readMoreBtn.addEventListener("click", () => {
+  moreText.classList.toggle("show");
 
-//         const url = URL.createObjectURL(file);
-
-//         if (file.type.startsWith("image")) {
-//             const img = document.createElement("img");
-//             img.src = url;
-//             img.style.width = "300px";
-//             img.style.borderRadius = "15px";
-//             preview.appendChild(img);
-//         } 
-//         else if (file.type.startsWith("video")) {
-//             const video = document.createElement("video");
-//             video.src = url;
-//             video.controls = true;
-//             video.style.width = "300px";
-//             preview.appendChild(video);
-//         }
-//     });
-// }
-
-
-
-
-document.querySelector(".primary").addEventListener("click", () => {
-  alert("CV download feature can be added later!");
+  if (moreText.classList.contains("show")) {
+    readMoreBtn.textContent = "Read Less";
+  } else {
+    readMoreBtn.textContent = "Read More";
+  }
 });
 
 
